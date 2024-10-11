c05345f469ccc7672beb181fc7cbf38959a78134
ammarikhan003 <ammar.ikhan003@gmail.com>
2024-10-11 17:00:47 +0100

    Activity 11

---
+++ src/components/entity/modules/ModuleForm.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import Icons from "../../UI/Icons.js";
+import { Button, ButtonTray } from "../../UI/Button.js";
+import Form from "../../UI/Form.js";
+
+const defaultModule = {
+  ModuleID: null, // Math.floor(100000 + Math.random() * 900000)
+  ModuleCode: null,
+  ModuleName: null,
+  ModuleLevel: null,
+  ModuleLeaderID: null,
+  ModuleLeaderName: null,
+  ModuleImage: null,
+};
+
+export const ModuleForm = ({ onSubmit, onCancel }) => {
+  // Initialisations ---------------------
+  defaultModule.ModuleID = Math.floor(100000 + Math.random() * 900000);
+  defaultModule.ModuleImage = "";
+
+  const levels = [
+    { value: 3, label: "3 (Foundation)" },
+    { value: 4, label: "4 (First year)" },
+    { value: 5, label: "5 (Second year)" },
+    { value: 6, label: "6 (Final year)" },
+    { value: 7, label: "7 (Masters)" },
+  ];
+
+  // State -------------------------------
+  const [module, setModule] = useState(defaultModule);
+
+  // Handlers ----------------------------
+  const handleChange = (field, value) =>
+    setModule({ ...module, [field]: value });
+  const handleSubmit = () => onSubmit(module);
+
+  // View --------------------------------
+  const submitLabel = "Add";
+  const submitIcon = <Icons.Add />;
+
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      onCancel={onCancel}
+      submitLabel={submitLabel}
+      submitIcon={submitIcon}
+    >
+      <Form.InputText
+        label="Module Code"
+        value={module.ModuleCode}
+        onChange={(value) => handleChange("ModuleCode", value)}
+      />
+
+      <Form.InputText
+        label="Module Name"
+        value={module.ModuleName}
+        onChange={(value) => handleChange("ModuleName", value)}
+      />
+
+      <Form.InputSelect
+        label="Module Level"
+        prompt="Select module level..."
+        options={levels}
+        value={module.ModuleLevel}
+        onChange={(value) => handleChange("ModuleLevel", value)}
+      />
+
+      <Form.InputText
+        label="Module Leader"
+        value={module.ModuleLeaderName}
+        onChange={(value) => handleChange("ModuleLeaderName", value)}
+      />
+
+      <Form.InputText
+        label="Module Image URL"
+        value={module.ModuleImage}
+        onChange={(value) => handleChange("ModuleImage", value)}
+      />
+
+      <ButtonTray>
+        <Button label={submitLabel} icon={submitIcon} onClick={handleSubmit} />
+        <Button label="Cancel" icon={<Icons.Close />} onClick={onCancel} />
+      </ButtonTray>
+      <Text>Add</Text>
+    </Form>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleAddScreen;
--- src/components/screens/ModuleAddScreen.js
+++ src/components/screens/ModuleAddScreen.js
@@ -1,108 +1,25 @@
-import { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import Screen from "../layout/Screen";
-import Icons from "../UI/Icons.js";
-import { Button, ButtonTray } from "../UI/Button.js";
-
-const defaultModule = {
-  ModuleID: null, // Math.floor(100000 + Math.random() * 900000)
-  ModuleCode: null,
-  ModuleName: null,
-  ModuleLevel: null,
-  ModuleLeaderID: null,
-  ModuleLeaderName: null,
-  ModuleImage: null,
-};
+import ModuleForm from "../entity/modules/moduleForm";
 
 export const ModuleAddScreen = (navigation, route) => {
   // Initialisations ---------------------
   const { onAdd } = route.params;
-  defaultModule.ModuleID = Math.floor(100000 + Math.random() * 900000);
-  defaultModule.ModuleImage = "";
 
   // State -------------------------------
-  const [module, setModule] = useState(defaultModule);
 
   // Handlers ----------------------------
-  const handleAdd = () => onAdd(module);
   const handleCancel = navigation.goBack;
-
-  const handleChange = (field, value) =>
-    setModule({ ...module, [field]: value });
 
   // View --------------------------------
 
   return (
     <Screen>
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module Code</Text>
-        <TextInput
-          value={module.ModuleCode}
-          onChangeText={(value) => handleChange("ModuleCode", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module Name</Text>
-        <TextInput
-          value={module.ModuleName}
-          onChangeText={(value) => handleChange("ModuleName", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module Level</Text>
-        <TextInput
-          value={module.ModuleLevel}
-          onChangeText={(value) => handleChange("ModuleLevel", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module Leader</Text>
-        <TextInput
-          value={module.ModuleLeaderName}
-          onChangeText={(value) => handleChange("ModuleLeaderName", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <View style={styles.item}>
-        <Text style={styles.itemLabel}>Module Image URL</Text>
-        <TextInput
-          value={module.ModuleImage}
-          onChangeText={(value) => handleChange("ModuleImage", value)}
-          style={styles.itemTextInput}
-        />
-      </View>
-
-      <ButtonTray>
-        <Button label="Add" icon={<Icons.Add />} onClick={handleAdd} />
-        <Button label="Cancel" onClick={handleCancel} />
-      </ButtonTray>
-      <Text>Add</Text>
+      <ModuleForm onSubmit={onAdd} onCancel={handleCancel} />
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({
-  itemLabel: {
-    color: "grey",
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  itemTextInput: {
-    height: 50,
-    paddingLeft: 10,
-    fontSize: 16,
-    backgroundColor: "white",
-    borderRadius: 7,
-    borderWidth: 1,
-    borderColor: "whitegray",
-  },
-});
+const styles = StyleSheet.create();
 
 export default ModuleAddScreen;
